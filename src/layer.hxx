7e2ba92a0f3a1770f82f5adaec2bb264fdbf3501
torph <73564623+Torphedo@users.noreply.github.com>
2025-10-05 11:20:55 -0400

    Add meta_layer

--- src/layer.hxx
+++ src/layer.hxx
@@ -16,3 +16,32 @@ struct gui_layer {
     /// Callback to run after the main loop
     virtual void destroy() {}
 };
+
+/// @brief A layer that can contain a bunch of other layers
+struct meta_layer : gui_layer {
+    std::vector<std::unique_ptr<gui_layer>> layers;
+
+    void init(GLFWwindow* window) override {
+        for (const auto& layer : layers) {
+            layer->init(window);
+        }
+    }
+
+    void update(GLFWwindow* window) override {
+        for (const auto& layer : layers) {
+            layer->update(window);
+        }
+    }
+
+    void render(GLFWwindow* window) override {
+        for (const auto& layer : layers) {
+            layer->render(window);
+        }
+    }
+
+    void destroy() override {
+        for (const auto& layer : layers) {
+            layer->destroy();
+        }
+    }
+};
